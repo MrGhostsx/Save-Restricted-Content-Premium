0981c2388b7060c2acfbeb29c7d577e47028233b
techshriyfra <techsashif+eufya@gmail.com>
2025-03-10 15:17:49 +0530

    Update strings.py

--- MrGhostsx/strings.py
+++ MrGhostsx/strings.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @Tech_Shreyansh29
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@techshreyansh
+# Ask Doubt on telegram @Tech_Shreyansh2
 
 HELP_TXT = """**🌟 Help Menu** 
 
@@ -31,6 +31,6 @@ https://t.me/c/xxxx/101 - 120
 
 __note that space in between doesn't matter__"""
        
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @Tech_Shreyansh29
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@techshreyansh
+# Ask Doubt on telegram @Tech_Shreyansh2
