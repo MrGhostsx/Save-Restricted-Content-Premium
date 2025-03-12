e029ed04e4713290a095048ba6cfcc23ca8bda94
Mr Ghost <s.rytgam.ing89@gmail.com>
2025-03-12 10:59:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,8 @@ API_ID = int(os.environ.get("API_ID", ""))
 # Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
 
+MONGO_URL = os.environ.get("MONGO_URL", "")
+
 # Your Owner / Admin Id For Broadcast 
 ADMINS = int(os.environ.get("ADMINS", "1234569875"))
 
